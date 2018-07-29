3a00d86c62eee0ce4983eb59d9bf12b94c1df1b2
asukachikaru <asukachikaru@gmail.com>
2018-07-29 22:33:31 +0900

    Added comment in js files

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 $(() => {
-  
+  //initialize socket.io  
   var socket = io();
 
   $textarea = $('textarea');
@@ -7,6 +7,7 @@ $(() => {
   $box = $('.box');
   $list = $('.list');
 
+  //decided username and login
   $('.login button').on('click', () => {
     login();
   });
@@ -15,34 +16,43 @@ $(() => {
   });
   
   function login() {
+    //invalid username(empty) handling
     if($('.login input').val() === '') {
       $('.login .warning').show();
     }else{
+      //emit login event and send username to server
       socket.emit('login', $('.login input').val());
+      //hide login page, show chatroom
       $('.login').hide();
       $('.container').css('display', 'flex');
       $('textarea').focus();
     }
   }
 
+  //boradcast login message
   socket.on('login', (username, userid) => {
     let $message = $(`<p class="sysM">${username} has joined this room</p>`);
     $box.append($message);
     $box.stop().animate({scrollTop: $box.scrollTop() + $message.offset().top}, 600);    
+    //add username to sidebar
     $list.append(`<p class="sysM" data-username="${userid}">${username}</p>`);
   });
+  //broadcast logout message
   socket.on('logout', (username, userid) => {
     let $message = $(`<p class="sysM">${username} has left this room</p>`);
     $box.append($message);
-    $box.stop().animate({scrollTop: $box.scrollTop() + $message.offset().top}, 600);    
+    $box.stop().animate({scrollTop: $box.scrollTop() + $message.offset().top}, 600); 
+    //remove username from sidebar   
     $list.find(`.sysM[data-username='${userid}']`).remove();
   });
 
+  //send message by enter
   $form.keypress((e) => {
     if($textarea.val() === '\n') $textarea.val('');    
     if(e.charCode === 13 && $textarea.val() !== '') $form.submit();      
   });
   
+  //send message
   $form.submit((e) => {
     e.preventDefault();
     if($textarea.val() !== ''){
@@ -52,6 +62,7 @@ $(() => {
     }
   });
 
+  //recieve message from server as my message
   socket.on('my message', (username, msg) => {
     msg = msg.replace('<', '&#60').replace('>', '&#62');
     let d = new Date();    
@@ -61,7 +72,7 @@ $(() => {
     $box.stop().animate({scrollTop: $box.scrollTop() + $message.offset().top}, 600);    
     $('textarea').focus();
   });
-
+  //recieve message from server as others message
   socket.on('others message', (username, msg) => {
     msg = msg.replace('<', '&#60').replace('>', '&#62');
     let d = new Date();    
--- server.js
+++ server.js
@@ -1,39 +1,49 @@
+//import dependencies
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+//client side static files
 app.use(express.static('public'));
+//initialize user object
 let users = {};
 
 io.on('connection', (socket) => {
   console.log(`User ${socket.id} has connected`);  
+  //add new user object by socket.id
   users[socket.id] = {socket: socket};  
     
+  //disconnection handling
   socket.on('disconnect', (socket) => {    
     for (const socket in users) {
       if(users[socket].socket.disconnected){
         console.log(`User ${users[socket].socket.id} has disconnected`);            
+        //if only user has logged in(decided username), broadcast logout message
         if(users[socket].hasOwnProperty('username')) io.emit('logout', users[socket].username, socket.substring(0,6));
+        //delete disconnected user from users object
         delete users[socket];
       } 
     }
   });
 
+  //when user logged in(decided username), save it to users object and broadcast login message
   socket.on('login', (username) => {
     io.emit('login', username, socket.id.substring(0,6));
     users[socket.id].username = username;    
   });
 
+  //message sent
   socket.on('send message', (msg) => {
-    // console.log(`${socket.id} sended message: ${msg}`);
-    
+    //loop over every user in users object. if user id is same, identify it as my message.
+    //else, identify it as others message
     Object.keys(users).forEach((socketid) => {
       if(socket.id == socketid){        
         eventid = 'my message';
       }else{
         eventid = 'others message'
       }
+      //broadcast message to everyone in chatroom
       if(users[socketid].hasOwnProperty('username')){
         io.to(socketid).emit(eventid, users[socket.id].username, msg);
       }
